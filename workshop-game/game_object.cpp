53242e641b4172c7bd94a4e4a8049584007b4e87
Henrique Jung <henriquenj@gmail.com>
2023-11-05 18:50:58 +0100

    PhysicalGameObject implementation
    Class will be used for entities containing a b2Body object.

--- workshop-game/game_object.cpp
+++ workshop-game/game_object.cpp
@@ -12,3 +12,53 @@ GameObject::GameObject(const GameContext* context)
 GameObject::~GameObject()
 {
 }
+
+void GameObject::Update()
+{
+}
+
+
+// PhysicalGameObject functions
+PhysicalGameObject::PhysicalGameObject(const GameContext* context)
+    : GameObject(context), body(nullptr)
+{
+}
+
+// Move constructor
+PhysicalGameObject::PhysicalGameObject(PhysicalGameObject&& other)
+    : GameObject(other.game_context)
+{
+    // copy pointer to b2Body and nullify other's body (so it is not deleted
+    // twice)
+    body = other.body;
+    other.body == nullptr;
+}
+
+// Move assigment operator
+PhysicalGameObject& PhysicalGameObject::operator=(PhysicalGameObject&& other)
+{
+    // prevent an object being moved onto itself
+    if (this != &other)
+    {
+        // if there's any body associated with this object, destroy it now
+        if (body != nullptr)
+        {
+            game_context->world->DestroyBody(body);
+        }
+
+        // copy the other body into this body and nullify the other's body
+        body = other.body;
+        other.body == nullptr;
+    }
+
+    return *this;
+}
+
+PhysicalGameObject::~PhysicalGameObject()
+{
+    if (body != nullptr)
+    {
+        // if there's a body defined for this game object, delete it now
+        game_context->world->DestroyBody(body);
+    }
+}
--- workshop-game/game_object.h
+++ workshop-game/game_object.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "game_context.h"
+#include "box2d/box2d.h"
 
 
 // GameObject class holds an instance of a b2Body and can process events on it
@@ -12,7 +13,33 @@ class GameObject
 public:
     GameObject(const GameContext* context);
     virtual ~GameObject();
-private:
+
+    // Called every frame
+    virtual void Update();
+protected:
     const GameContext* game_context;
-    b2Body* object;
+};
+
+
+// A GameObject that also contains a b2Body
+class PhysicalGameObject : public GameObject
+{
+
+    // Main constructor
+    PhysicalGameObject(const GameContext* context);
+
+    // Copy constructor and copy assigment are deleted for now.
+    // TODO: implement those
+    PhysicalGameObject(const PhysicalGameObject& other) = delete;
+    PhysicalGameObject& operator=(const PhysicalGameObject& other) = delete;
+
+    // Move constructor
+    PhysicalGameObject(PhysicalGameObject&& other);
+
+    // Move assigment operator
+    PhysicalGameObject& operator=(PhysicalGameObject&& other);
+
+    virtual ~PhysicalGameObject();
+protected:
+    b2Body* body;
 };
